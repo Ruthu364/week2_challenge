b7f1029afc0c68b9abdffb6381081e4d60723d1e
Ruthu364 <121491563+Ruthu364@users.noreply.github.com>
2024-04-30 00:01:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-import React, { useState, useEffect, useContext, useReducer, useCallback } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useContext, useReducer } from 'react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 // Counter context
